840a51ee76931d37f8d75949b7a45423502a6276
jaso081529 <227775566+jaso081529@users.noreply.github.com>
2026-07-12 14:45:03 +0200

    Artikelnamen auf Etiketten lesbar umbrechen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hp67-v23';
+const CACHE='hp67-v24';
 const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./icon.svg','./vendor/html5-qrcode.min.js','./vendor/jsbarcode.min.js','./vendor/xlsx.full.min.js','./vendor/jspdf.umd.min.js','./vendor/jspdf.plugin.autotable.min.js','./vendor/jszip.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
